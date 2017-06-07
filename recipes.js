fa9b1f258656143aa435ec39329bb8fb2696b8d5
brandwand <brandwand@yahoo.com>
2017-06-07 12:12:34 -0600

    Did stuff

--- recipes.js
+++ recipes.js
@@ -1,5 +1,10 @@
+
+var total = 0;
+
+
+
 function checkvalue(fieldvalue) {
-    switch(fieldvalue){
+    switch (fieldvalue) {
         case 1:
             document.getElementById("image").innerHTML = "<img src ='images/hawaiian.png'>";
             break;
@@ -15,4 +20,53 @@ function checkvalue(fieldvalue) {
         case 5:
             document.getElementById("image").innerHTML = "<img src ='images/veggie.png'>";
     }
+
+
+}
+function loadRecipe(recipevalue) {
+    var recipePrice = 0;
+    switch (recipevalue) {
+        case 1:
+            document.getElementById("list").innerHTML = "Ham,Pineapple,Bacon,Red Pepper";
+            recipePrice = 3;
+            break;
+        case 2:
+            document.getElementById("list").innerHTML = "Bacon,Ham,Pepperoni,Sausage";
+            recipePrice = 3;
+            break;
+        case 3:
+            document.getElementById("list").innerHTML = "Pepperoni,Sausage,Green Peppers,Mushrooms,Onions";
+            recipePrice = 3;
+            break;
+        case 4:
+            document.getElementById("list").innerHTML = "Ham,Pepperoni,Sausage,Onions,Green Peppers,Mushrooms,Olives";
+            recipePrice = 5;
+            break;
+        case 5:
+            document.getElementById("list").innerHTML = "Green Peppers,Mushrooms,Onions,Red Peppers";
+            recipePrice = 3;
+            break;
+    }
+    total += recipePrice;
+}
+
+function sizePrice(sizevalue) {
+    var sizePrices = 0;
+
+    switch (sizevalue) {
+
+        case 1:
+            sizePrices = 4;
+            break;
+        case 2:
+            sizePrices = 8;
+            break;
+        case 3:
+            sizePrices = 12;
+            break;
+        case 4:
+            sizePrices = 16;
+            break;
+    }
+    total += sizePrices;
 }
