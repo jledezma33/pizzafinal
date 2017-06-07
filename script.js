0c1c0f5834b1053afaa40d16158e664799c33d4f
brandwand <brandwand@yahoo.com>
2017-06-06 18:53:10 -0600

    blah

---
+++ script.js
@@ -0,0 +1,283 @@
+function MyBacon(){
+     var img = document.getElementById('baconImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function MyBaconLeft(){
+     var img = document.getElementById('baconLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function MyBaconRight(){
+     var img = document.getElementById('baconRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+
+function GreenPepper(){
+     var img = document.getElementById('GreenPepperImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function GreenPepperLeft(){
+     var img = document.getElementById('GreenPepperLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function GreenPepperRight(){
+     var img = document.getElementById('GreenPepperRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function Ham(){
+     var img = document.getElementById('HamImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function HamLeft(){
+     var img = document.getElementById('HamLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function HamRight(){
+     var img = document.getElementById('HamRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+  
+}
+function Mushroom(){
+      var img = document.getElementById('MushroomImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function MushroomL(){
+      var img = document.getElementById('MushroomLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function MushroomR(){
+      var img = document.getElementById('MushroomRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function Olive(){
+          var img = document.getElementById('OliveImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function OliveLeft(){
+          var img = document.getElementById('OliveLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function OliveRight(){
+          var img = document.getElementById('OliveRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+
+function Onion(){
+          var img = document.getElementById('OnionImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function OnionLeft(){
+          var img = document.getElementById('OnionLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function OnionRight(){
+          var img = document.getElementById('OnionRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function Pepporoni(){
+   var img = document.getElementById('PepperoniImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function PepporoniLeft(){
+   var img = document.getElementById('PepperoniLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function PepporoniRight(){
+   var img = document.getElementById('PepperoniRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function Pineapple(){
+      var img = document.getElementById('PineappleImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function PineappleLeft(){
+      var img = document.getElementById('PineappleLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function PineappleRight(){
+      var img = document.getElementById('PineappleRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+
+function RedPepper(){
+         var img = document.getElementById('RedPepperImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+
+function RedPepperLeft(){
+         var img = document.getElementById('RedPepperLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function RedPepperRight(){
+         var img = document.getElementById('RedPepperRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function Sausage(){
+    var img = document.getElementById('SausageImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function SausageLeft(){
+    var img = document.getElementById('SausageLImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
+function SausageRight(){
+    var img = document.getElementById('SausageRImg');
+     if(img.style.visibility === 'visible'){
+         img.style.visibility = "hidden";
+     }
+     else{
+         img.style.visibility = "visible";
+     }
+}
