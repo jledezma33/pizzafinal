41091fc4fb77ab2399ddb8794e1a41769dfbf66a
Jesus Ledezma <jledezma@student.neumont.edu>
2017-06-06 20:26:37 -0600

    Got the images working on the recipe page.

---
+++ recipes.js
@@ -0,0 +1,19 @@
+function checkvalue(fieldvalue) {
+    switch(fieldvalue){
+        case 1:
+            document.getElementById("image").innerHTML = "<img src ='images/hawaiian.png'>";
+            break;
+        case 2:
+            document.getElementById("image").innerHTML = "<img src ='images/meatlovers.png'>";
+            break;
+        case 3:
+            document.getElementById("image").innerHTML = "<img src ='images/supreme.png'>";
+            break;
+        case 4:
+            document.getElementById("image").innerHTML = "<img src ='images/supersupreme.png'>";
+            break;
+        case 5:
+            document.getElementById("image").innerHTML = "<img src ='images/veggie.png'>";
+            break;
+    }
+}
